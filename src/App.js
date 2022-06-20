e31f04113ca50183d2da9a25767775cf2896b71d
SHARKZTECH <87173929+SHARKZTECH@users.noreply.github.com>
2022-06-20 07:44:14 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import MetaConnect from "./MetaConnect";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Account Connection</h1>
+      <MetaConnect />
+    </div>
+  );
+}
